9377fbc06ff5724393cbea76ba7073cfbb0409cc
ImagClaw <dal.whelpley@gmail.com>
2019-03-19 14:11:28 -0500

    updated perPrompt1.c with key req

--- prePrompt1.c
+++ prePrompt1.c
@@ -9,23 +9,36 @@
 #include <stdio.h>
 
 int main() {
-    const char userInput[10000];
-    const char searchStr[64];
+    const char userInput[10001];
+    const char searchStr[65];
     char *ret;
     char *ret2;
-
-    printf("Enter an initial string: ");
+    char key[4];
+    
+    // Enter an initial string (max 10000 chars)
+    printf("\n\t Enter an initial string: ");
     scanf("%[^\n]s", userInput);
-
-    printf("Enter a search string: ");
+    
+    // Enter the search term (max 64 chars)
+    printf("\n\t Enter a search string: ");
     scanf("%s", searchStr);
+        
+    // Enter the pass key to continue
+    printf("\n\t Enter the password to continue: ");
+    scanf("%s", key);
 
-    ret = strstr(userInput, searchStr);
-    printf("\n\n\nFirst Occurance: %s\n", ret);
-
-    ret2 = strstr(ret + 1, searchStr);
-    printf("\n\n\nSecond Occurance: %s\n", ret2);
-
-    return 0;
-
+    if (strcmp(key, "idf") != 0) {
+        printf("\n\t WRONG PASSWORD! Try again.");
+        main();
+    } else {
+        ret = strstr(userInput, searchStr);
+        printf("\n\t First Occurance: %s\n", ret);
+    
+        ret2 = strstr(ret + 1, searchStr);
+        if (ret2 != NULL) {
+            printf("\n\t Second Occurance: %s\n", ret2);
+        } else {
+            printf("\n\t There is no second occurance.");
+        }
+    }
 }
